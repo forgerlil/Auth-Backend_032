f17ee8d7687ccd044c18de7aeedc5e136a83aa57
forgerlil <lilianforger@gmail.com>
2022-12-16 10:33:12 +0100

    Added duck schema and controllers

--- controllers/duckControllers.js
+++ controllers/duckControllers.js
@@ -5,22 +5,23 @@ const getDucks = async (req, res) => {
     const allDucks = await duckCollection.find();
     return res.json(allDucks);
   } catch (err) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: err.message });
   }
 };
 
 const addDuck = async (req, res) => {
   try {
-    const { name, image } = req.body;
-    if (!name || !image) throw new Error('Please send in all fields');
+    const { name, image, owner } = req.body;
+    if (!name || !image || !owner) throw new Error('Please send in all fields');
 
     const findDuck = await duckCollection.findOne({ name });
     if (findDuck) throw new Error('Duck already exists');
 
-    const newDuck = await duckCollection.create({ name, image });
-    return res.status(201).json(newDuck);
+    const newDuck = await duckCollection.create({ name, image, owner });
+    const allDucks = await duckCollection.find();
+    return res.status(201).json(allDucks);
   } catch (err) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: err });
   }
 };
 
--- models/duckSchema.js
+++ models/duckSchema.js
@@ -1,8 +1,9 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
-const duckSchema = new Schema({
+const duckSchema = new mongoose.Schema({
   name: { type: String, unique: true, required: true },
   image: { type: String, required: true },
+  owner: { type: mongoose.Mixed, firstName: String, lastName: String },
 });
 
-module.exports = model('Duck', duckSchema);
+module.exports = mongoose.model('Duck', duckSchema);
